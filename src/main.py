cd51844c4e4f14929b56f26d11ca03a668738726
Nicolas Gross <nicolas@fam-gross.org>
2019-06-24 17:32:11 +0200

    Add main python file

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    ui_file = QFile("mainwindow.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    # init
+
+    window.show()
+
+    sys.exit(app.exec_())
+
