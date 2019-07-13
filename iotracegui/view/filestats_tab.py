e895fbbdd70570c7adfb624fc43f3727ce057e1a
Nicolas Gross <nicolas@fam-gross.org>
2019-07-13 19:33:25 +0200

    Indicate invalid regexs

--- iotracegui/view/filestats_tab.py
+++ iotracegui/view/filestats_tab.py
@@ -1,4 +1,4 @@
-from PySide2.QtCore import Qt, Slot
+from PySide2.QtCore import Qt, Slot, QRegularExpression
 
 
 class FilestatsTab:
@@ -6,8 +6,8 @@ class FilestatsTab:
         self.__window = window
         self.__model = model
         self.__model.filesLoaded.connect(self.__refreshData)
+        self.__window.filestatsLineEdit.textChanged.connect(self.__checkRegex)
         self.__refreshData()
-        # TODO red invalid regex
 
     @Slot()
     def __refreshData(self):
@@ -24,3 +24,13 @@ class FilestatsTab:
         selectedProc = self.__model.procsModel.data(current, Qt.ItemDataRole)
         filestatModel = self.__model.getFilestatModel(selectedProc)
         self.__window.filestatsTableView.setModel(filestatModel)
+
+    @Slot()
+    def __checkRegex(self, pattern):
+        regex = QRegularExpression(pattern)
+        if regex.isValid():
+            self.__window.filestatsLineEdit.setStyleSheet(
+                    "background-color: white;")
+        else:
+            self.__window.filestatsLineEdit.setStyleSheet(
+                    "background-color: orange;")
